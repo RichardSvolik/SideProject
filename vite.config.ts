df66a90008bdadacaa2960cf6692639645c8bef4
Richard Svolik <richard.svolik@sap.com>
2023-10-09 09:55:29 +0200

    untrusted ssl install

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//     server: {port: 3000, https: true}
+// })
+
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-    server: {port: 3000}
-})
+export default {
+  // server: { https: true },
+  plugins: [
+    basicSsl(),
+    react(),
+  ]
+}
