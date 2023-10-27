ff986ac3118906f1a257b91135c5b4e828201dca
Richard Svolik <richard.svolik@sap.com>
2023-10-27 09:17:57 +0200

    added list of Items on add item page

--- src/components/ListOfItems.jsx
+++ src/components/ListOfItems.jsx
@@ -1,84 +1,68 @@
-import * as React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
-import Checkbox from "@mui/material/Checkbox";
 import Avatar from "@mui/material/Avatar";
-import { Divider, Typography } from "@mui/material";
+import { Divider, Typography, Box, Link } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-function ListOfItems({ items }) {
-  const [checked, setChecked] = React.useState([1]);
+function ListOfItems({ items, setItems }) {
+  const handleDeleteItem = (itemName) => {
+    JSON.parse(localStorage.getItem("items"));
+    console.log("itemsInLocalStorage: ", items);
+
+    const newItems = items.filter((item) => item.itemName !== itemName);
+    console.log("newItems: ", newItems);
+    localStorage.clear();
+    localStorage.setItem("items", JSON.stringify(newItems));
+    setItems(JSON.parse(localStorage.getItem("items")));
+    setItems(newItems);
+  };
 
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Brunch this weekend?"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: "inline" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                Ali Connors
-              </Typography>
-              {" — I'll be in your neighborhood doing errands this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Summer BBQ"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: "inline" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                to Scott, Alex, Jennifer
-              </Typography>
-              {" — Wish I could come, but I'm out of town this…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-      <Divider variant="inset" component="li" />
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        </ListItemAvatar>
-        <ListItemText
-          primary="Oui Oui"
-          secondary={
-            <React.Fragment>
-              <Typography
-                sx={{ display: "inline" }}
-                component="span"
-                variant="body2"
-                color="text.primary"
-              >
-                Sandra Adams
-              </Typography>
-              {" — Do you have Paris recommendations? Have you ever…"}
-            </React.Fragment>
-          }
-        />
-      </ListItem>
-    </List>
+    <Box sx={{ flex: 4, padding: 2 }}>
+      {items.map((item, index) => (
+        <List
+          key={index}
+          sx={{ maxWidth: "100%", bgcolor: "background.paper" }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                sx={{ maxWidth: "fit-content" }}
+                variant="rounded"
+                alt={item.itemName}
+                src={item.itemImage}
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary={item.itemName}
+              secondary={
+                <Typography
+                  sx={{ display: "inline" }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  <Link href={item.itemLink} target="_blank" underline="none">
+                    {item.itemLink}
+                  </Link>
+                </Typography>
+              }
+            />
+            <IconButton
+              edge="end"
+              aria-label="delete"
+              onClick={() => handleDeleteItem(item.itemName)}
+            >
+              <DeleteIcon />
+            </IconButton>
+          </ListItem>
+          <Divider variant="inset" component="li" />
+        </List>
+      ))}
+    </Box>
   );
 }
 
