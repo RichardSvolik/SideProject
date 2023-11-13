cf9f31d4247da73734f59bc0f716e91c8af774fa
Richard Svolik <richard.svolik@sap.com>
2023-11-13 14:07:55 +0100

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,13 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5175/',
+
+    baseURL: 'https://localhost:5173/',
+    ignoreHTTPSErrors: true,
+    launchOptions: {
+      args: ['--ignore-certificate-errors']
+  },
+
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
