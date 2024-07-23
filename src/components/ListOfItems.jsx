351b18da18d9e623ef01d34016e9f65d0d9556f8
Richard Svolik <richard.svolik@sap.com>
2024-07-23 12:30:20 +0200

    removed unused imports

--- src/components/ListOfItems.jsx
+++ src/components/ListOfItems.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect } from "react";
+import { useContext } from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import { Button } from "@mui/material";
